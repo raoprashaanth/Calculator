c49c885c5f8db72e591fad987d8e8619733494b1
HOME <HOME@192.168.100.11>
2017-05-25 05:34:31 -0700

    local variable

---
+++ src/com/onlinecalculator/calculator/calctypes/LocalVariable.java
@@ -0,0 +1,19 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class LocalVariable {
+
+	 static void m1()
+	{
+		int a=5, b=10;
+		System.out.println("local variable a:"+a);
+		System.out.println("local variable b:"+b);
+	}
+	public static void main(String[] args)
+	{
+		m1();
+	}
+}
+
+
